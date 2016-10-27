dae33e19a8cba291e9ed071387be77f465b51705
kabamawekesa <sinlaoej@live.com>
2016-10-27 12:21:34 -0400

    Update Querry.sh

--- Querry.sh
+++ Querry.sh
@@ -1,8 +1,10 @@
+
 #!/bin/bash
 
+
+
 selection=
-until [ "$selection" = "0" ]; 
-do
+until [ "$selection" = "0" ]; do
     echo ""
     echo "Advanced Queries List Menu"
     echo "1 - List all players who play for a certain team and their positions"
@@ -13,99 +15,104 @@ do
     echo ""
     echo "0 - exit program"
     echo ""
-    echo -n "Please Enter a Selection: "
+    echo -n "Enter selection: "
     read selection
     echo ""
     case $selection in
+ 1 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 
-        1 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
+user=esinlao
+pass=04177544
 
-            user=esinlao
-            pass=04177544
+sqlplus64 "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
-            sqlplus64 -s "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+SELECT Team_Name, Player_Name, Position
+        FROM Plays_For, Team, Player
+        WHERE Team.Team_ID = 01
+                AND Plays_For.Team_ID = Team.Team_ID
+                AND Plays_For.Player_ID = Player.Player_ID
+        ORDER BY Position ASC;
 
-            SELECT Team_Name, Player_Name, Position
-                    FROM Plays_For, Team, Player
-                    WHERE Team.Team_ID = 01
-                         AND Plays_For.Team_ID = Team.Team_ID
-                         AND Plays_For.Player_ID = Player.Player_ID
-            ORDER BY Position ASC;
+exit;
 
-            exit;
-            EOF;;
-    
-        2 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
+EOF
+   o
+                                               ;; 
+2 )export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 
-            user=esinlao
-            pass=04177544
+user=esinlao
+pass=04177544
 
-            sqlplus64 -s "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+sqlplus64 "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
-            SELECT Coach_Name, Team_Name
+SELECT Coach_Name, Team_Name
                 FROM Coach, Team, Manages
                 WHERE Team.Team_ID = 01
-                    AND Manages.Team_ID = Team.Team_ID
-                    AND Manages.Coach_License_Number = Coach.Coach_License_Number
+                AND Manages.Team_ID = Team.Team_ID
+                AND Manages.Coach_License_Number = Coach.Coach_License_Number
                 ORDER BY Coach_Name ASC;
 
-            exit;
-            EOF;;
-            
-        3 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
+exit;
 
-            user=esinlao
-            pass=04177544
+EOF
+                                             ;;
+ 3 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 
-            sqlplus64 -s "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+user=esinlao
+pass=04177544
 
-            SELECT Team_Name, COUNT(Player_ID) AS Number_of_Players
+sqlplus64 "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+
+SELECT Team_Name, COUNT(Player_ID) AS Number_of_Players
                 FROM Team, Plays_For
                 WHERE Team.Team_ID = Plays_For.Team_ID
                 GROUP BY Team_Name
                 ORDER BY Team_Name ASC;
+exit;
 
-            exit;
-            EOF;;
-            
-        4 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
+EOF
+                                                ;;
+ 4 )export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 
-            user=esinlao
-            pass=04177544
+user=esinlao
+pass=04177544
 
-            sqlplus64 -s "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+sqlplus64 "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
-            SELECT Player_Name, Goals, Saves
+        SELECT Player_Name, Goals, Saves
                 FROM Player
                 WHERE Goals > 10
                 UNION
                 (SELECT Player_Name, Goals, Saves
                 FROM Player
                 WHERE Saves > 10);
-                
-            exit;
-            EOF;;
-            
-        5 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
+exit;
 
-            user=esinlao
-            pass=04177544
-    
-            sqlplus64 -s "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+EOF
+                                                ;;
+ 5 ) export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 
-            SELECT Team_Name, Player_Name, Contract_Expiration_Date
+user=esinlao
+pass=04177544
+
+sqlplus64 "$user/$pass@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=141.117.57.159)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+
+SELECT Team_Name, Player_Name, Contract_Expiration_Date
                 FROM Plays_For, Team, Player
                 WHERE Team.Team_ID = 01
-                    AND Plays_For.Team_ID = Team.Team_ID
-                    AND Plays_For.Player_ID = Player.Player_ID
+                AND Plays_For.Team_ID = Team.Team_ID
+                AND Plays_For.Player_ID = Player.Player_ID
                 ORDER BY Contract_Expiration_Date ASC;
-                
-            exit;
-            EOF;;
-            
+exit;
+
+EOF
+                                                ;;
+
         0 ) exit ;;
-        
-        * ) echo "Invalid option."
-        
+        * ) echo "Please enter 1, 2, 3, 4, 5, or 0"
     esac
 done
+
+
+
+
